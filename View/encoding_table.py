ccf3cd4e2fc8bfae6dd95c581fb11e3a2829c26e
Briggs-Richardson <briggsbdr@gmail.com>
2022-11-18 17:06:23 -0700

    Solved merge conflict errors

--- View/encoding_table.py
+++ View/encoding_table.py
@@ -1,57 +1,6 @@
-from PySide6.QtWidgets import QTableWidget, QTableWidgetItem
 from PySide6.QtCore import QSettings
-from PySide6 import QtWidgets
-from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QTableWidget, QTableWidgetItem, QLineEdit
-from PySide6 import QtWidgets, QtGui, QtCore
-<< << << < HEAD
-
-
-class EncodingTable(QTableWidget):
-    """
-    EncodingTable is a custom QTableWidget used to support encoding table
-    functionalities in the front-end.
-    """
-
-    def __init__(self):
-        """
-        Constructor - Sets the properties of a QTableWidget.
-        """
-        super().__init__()
-
-        self.setRowCount(10)
-        self.setColumnCount(4)
-
-        self.horizontalHeader().setStretchLastSection(True)
-        self.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Interactive)
-        self.horizontalHeader().setDefaultSectionSize(60)
-        self.horizontalHeader().setMaximumSectionSize(400)
-        self.verticalHeader().setStretchLastSection(True)
-        self.verticalHeader().setSectionResizeMode(
-            QtWidgets.QHeaderView.ResizeToContents)
-        self.verticalHeader().setDefaultSectionSize(30)
-        self.setStyleSheet("QTableWidget::item {border: 0px; padding: 5px;}")
-
-        """Ensure at least 5 rows are visible at all times."""
-        self.minimum_visible_rows = 5
-        total_border_height = 2
-        self.setMinimumHeight(self.rowHeight(
-            0) * (self.minimum_visible_rows + total_border_height))
-
-    def add_column(self):
-        """Increases the column count of the table by 1."""
-        self.setColumnCount(self.columnCount() + 1)
-
-        """Creates QTableWidgetItem for new column"""
-        column = self.columnCount() - 1
-        self.setHorizontalHeaderItem(column, QTableWidgetItem(str(column)))
-
-    def add_row(self):
-        """Increases the row count of the table by 1."""
-        self.setRowCount(self.rowCount() + 1)
-
-
-== == == =
+from PySide6 import QtWidgets, QtCore
 
 
 class EncodingTable(QTableWidget):
@@ -248,6 +197,3 @@ class EncodingTable(QTableWidget):
         settings.endGroup()  # table-data
         settings.endGroup()  # encoding-table
         settings.endGroup()  # session-id
-
-
->>>>>> > main
