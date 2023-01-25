a3b316a9737a5fa1ea62e47e678e86a4147d7f97
Jacob-Hensley <jchensle@asu.edu>
2023-01-25 15:55:38 -0800

    Add Buttons to Coding Assistance Panel
    Adds a change to the Coding Assistance Panel where the "+" button now opens a dialog that allows the user add a new button to the Coding Assistance Panel. The dialog allows the user to label the button and assign a hotkey to the button. Hotkeys cannot be reused.
    
    Testing Steps
      1. Open the Application and start a new session
      2. Click the + button under "Coding Assistance Buttons"
      3. Give a title and a hotkey "a" to the button and press the "Create" button
      4. Verify the button has been added and has the correct label
      5. Press the "Create" button again without changing the parameters and verify the text "This hotkey is already being used!" shows in the dialog
      6. Change the hotkey to "s" and click the "Create" button
      7. Verify a new button is created.
    
    Type: New Feature

--- Controllers/controller.py
+++ Controllers/controller.py
@@ -1,10 +1,11 @@
 import sys
 
-from PySide6.QtCore import Slot, QMimeDatabase
+from PySide6.QtCore import Slot, QMimeDatabase, QObject
 from PySide6.QtMultimedia import QMediaFormat, QMediaPlayer
-from PySide6.QtWidgets import QFileDialog, QDialog, QStyle
+from PySide6.QtWidgets import QFileDialog, QDialog, QStyle, QPushButton
 
 from View.user_settings_dialog import UserSettingsDialog
+from View.coding_assistance_button_dialog import CodingAssistanceButtonDialog
 
 
 def get_supported_mime_types():
@@ -82,6 +83,9 @@ class Controller:
 
         self._window.table_panel.add_col_button.clicked.connect(self.add_col_to_encoding_table)
         self._window.table_panel.add_row_button.clicked.connect(self.add_row_to_encoding_table)
+
+        self._window.coding_assistance_panel.button_panel.connect_add_button_to_slot(self.open_coding_assistance_dialog)
+        #self._window.coding_assistance_panel.button_panel.connect_new_button_to_slot(self.coding_assistance_button_operation)
 
     @Slot()
     def add_col_to_encoding_table(self):
@@ -327,3 +331,36 @@ class Controller:
             button.setIcon(button.style().standardIcon(QStyle.SP_MediaPause))
         else:
             button.setIcon(button.style().standardIcon(QStyle.SP_MediaPlay))
+
+    @Slot()
+    def open_coding_assistance_dialog(self):
+        """Open a dialog to create a new Coding Assistance Button"""
+        self.coding_assistance_button_dialog = CodingAssistanceButtonDialog()
+        self.coding_assistance_button_dialog.connect_create_button_to_slot(self.create_coding_assistance_button)
+        self.coding_assistance_button_dialog.exec()
+
+    @Slot()
+    def create_coding_assistance_button(self):
+        """Add a button to the Coding Assistance Panel"""
+        button_title = self.coding_assistance_button_dialog.apply_text_field.text()
+        button_hotkey = self.coding_assistance_button_dialog.hotkey_field.text()
+
+        if len(self.coding_assistance_button_dialog.hotkeys) == 0:
+            self.coding_assistance_button_dialog.hotkeys.append(button_hotkey)
+            self._window.coding_assistance_panel.button_panel.create_coding_assistance_button(button_title,
+                                                                                              button_hotkey)
+        else:
+            for hotkey in self.coding_assistance_button_dialog.hotkeys:
+                if hotkey == button_hotkey:
+                    self.coding_assistance_button_dialog.error_label.setText("This hotkey is already being used!")
+                    break
+                else:
+                    self.coding_assistance_button_dialog.hotkeys.append(button_hotkey)
+                    self._window.coding_assistance_panel.button_panel.create_coding_assistance_button(button_title,
+                                                                                                      button_hotkey)
+
+
+    @Slot()
+    def coding_assistance_button_operation(self):
+        """Set the data a Coding Assistance Button will input into the table"""
+
