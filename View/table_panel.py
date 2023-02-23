ef9b42768f87079cfc7f599b01634d7e11ea6ced
Yagotshus99 <snunna2@asu.edu>
2023-02-23 15:53:26 -0700

    Added my changes to a branch based of most recent main at the moment.
    Moved my changes to this branch to lower number of merge conflicts and to lower the chance of blundering fixing merge conflicts.

--- View/table_panel.py
+++ View/table_panel.py
@@ -1,6 +1,7 @@
 from PySide6 import QtCore
 from PySide6.QtWidgets import QWidget, QPushButton, QGridLayout, QSizePolicy, QComboBox, QLabel, QLineEdit
 from View.encoding_table import EncodingTable
+from PySide6.QtCore import Qt
 
 
 class TablePanel(QWidget):
@@ -21,6 +22,8 @@ class TablePanel(QWidget):
 
         self.add_col_button = QPushButton("Add column")
         self.add_row_button = QPushButton("Add row")
+        self.delete_col_button = QPushButton("Delete current column")
+        self.delete_row_button = QPushButton("Delete current row")
 
         # Configure the add column button to expand vertically.
         self.add_col_button.setSizePolicy(
@@ -37,11 +40,12 @@ class TablePanel(QWidget):
         grid_layout = QGridLayout()
         grid_layout.addWidget(self.title, 0, 0, QtCore.Qt.AlignCenter)
         grid_layout.addWidget(self.table, 1, 0)
-        grid_layout.addWidget(self.add_col_button, 2, 1)
-        grid_layout.addWidget(self.add_row_button, 2, 0)
-        grid_layout.addWidget(self.change_font_label, 1, 1)
-        grid_layout.addWidget(self.change_font_dropDown, 1, 2)
-
+        grid_layout.addWidget(self.add_col_button, 1, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(self.add_row_button, 1, 1, alignment=Qt.AlignBottom)
+        grid_layout.addWidget(self.change_font_label, 1, 1, alignment=Qt.AlignTop)
+        grid_layout.addWidget(self.change_font_dropDown, 1, 2, alignment=Qt.AlignTop|Qt.AlignLeft)
+        grid_layout.addWidget(self.delete_col_button, 1, 2, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(self.delete_row_button, 1, 2, alignment=Qt.AlignBottom)
         self.setLayout(grid_layout)
 
     def get_table_name(self):
