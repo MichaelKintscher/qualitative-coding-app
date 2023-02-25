1d75c9c31bd6b17d94560b725f182a231f63d419
Briggs-Richardson <briggsbdr@gmail.com>
2023-02-25 12:32:53 -0700

    Adds a delete row and delete column button
    In this patch, a user can select a row or column and then press the "delete
    selected row" or "delete selected column" button to delete the selected
    row/column. The buttons also work if a user selects a specific cell
    instead of a whole row/column. The first row and first column cannot
    be deleted currently.
    
    Testing Steps:
      1. Run main.py to start the application.
      2. Select a column (not the first) and press the delete column button, make
         sure column is deleted.
      3. Select the first column, press the delete button, and make sure it is
         NOT deleted.
      4. Select a row (not the first) and press the delete row button make sure
         row is deleted.
      5. Verify that the last row of the table cannot be deleted.
      6. Select a column and click the delete row button, make sure nothing is
         deleted.
      7. Delete all rows but one, then select the only row left and click the
         delete row button, make sure it is NOT deleted.
    
    Type: New Feature

--- View/encoding_table.py
+++ View/encoding_table.py
@@ -68,6 +68,50 @@ class EncodingTable(QTableWidget):
         """
         self.setRowCount(self.rowCount() + 1)
 
+    def del_column(self):
+        """
+        Deletes current selected column
+        """
+        current_col = self.currentColumn()
+        item_list = self.selectedIndexes()
+        if current_col != 0 and len(item_list) > 0:
+            self.removeColumn(current_col)
+
+    def del_row(self):
+        """
+        Deletes current selected row
+        """
+        if self.rowCount() > 1:
+            current_row = self.currentRow()
+            item_list = self.selectedIndexes()
+            num_columns = self.columnCount()
+            num_rows = self.rowCount()
+            row = col = False
+
+            # This logic determines whether a column or a row is selected
+            if num_columns == 1:
+                if len(item_list) == 0:
+                    return
+                elif len(item_list) == num_rows:
+                    col = True
+                else:
+                    row = True
+            elif len(item_list) > 1:
+                row1 = item_list[0].row()
+                col1 = item_list[0].column()
+                row2 = item_list[1].row()
+                col2 = item_list[1].column()
+
+                if row1 == row2 and col1 != col2:
+                    row = True
+                if row1 != row2 and col1 == col2:
+                    col = True
+
+            if current_row == -1 or col:
+                return
+            elif row:
+                self.removeRow(current_row)
+
     def change_font(self, font_choice):
         """
         Changes font size of cells.
