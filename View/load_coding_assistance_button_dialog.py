87c0f8553e6a7b3b81e2d6b05360263e3012cb94
Jcobo314 <94135853+Jcobo314@users.noreply.github.com>
2023-03-29 20:51:29 -0700

    Load a Single Button with a Hotkey
    This patch changes the UI of the load coding assistance button dialog to ensure a user can only load a single button at a time and allow the user to assign a new hotkey to the button loaded.
    
    Testing Steps:
      1. Create a new session
      2. Load a video file
      3. Click "+" then create and save a new button with hotkey "a" assigned
      4. Close the dialog and ensure the hotkey activates the button created
      5. Click "+" then create and save a new button with no hotkey
      6. Click "Load Button"
      7. Ensure that you can only select one of the two buttons in the list
      8. Select the first button and assign hotkey "b"
      9. Click "Load Button" and close the dialogs until back to the main panel.
      10. Ensure that the button loaded has the same name as the first and that hotkey "a" activates the first button and 
             hotkey "b" activates the second button
    
    Type: Bug Fix

--- View/load_coding_assistance_button_dialog.py
+++ View/load_coding_assistance_button_dialog.py
@@ -1,6 +1,4 @@
-from PySide6.QtWidgets import QDialog, QCheckBox, QVBoxLayout, QPushButton
-
-from Models.button_definition_entity import ButtonDefinitionEntity
+from PySide6.QtWidgets import QDialog, QCheckBox, QVBoxLayout, QPushButton, QHBoxLayout, QLabel, QLineEdit, QRadioButton
 
 
 class LoadCodingAssistanceButtonDialog(QDialog):
@@ -12,15 +10,25 @@ class LoadCodingAssistanceButtonDialog(QDialog):
         super().__init__()
 
         dialog_layout = QVBoxLayout()
-        self.checkboxes = []
+        self.radio_buttons = []
 
         for button_definition in button_definitions:
-            checkbox = QCheckBox("Button Name: " + button_definition.button_id)
-            self.checkboxes.append(checkbox)
-            dialog_layout.addWidget(checkbox)
+            radio_button = QRadioButton("Button Name: " + button_definition.button_id)
+            self.radio_buttons.append(radio_button)
+            dialog_layout.addWidget(radio_button)
 
-        self.load_button = QPushButton("Load Buttons Selected")
+        self.load_button = QPushButton("Load Button")
 
+        hotkey_hbox = QHBoxLayout()
+        hotkey_label = QLabel("Assign a hotkey to this button")
+        self.hotkey_textfield = QLineEdit()
+        hotkey_hbox.addWidget(hotkey_label)
+        hotkey_hbox.addWidget(self.hotkey_textfield)
+
+        self.error_label = QLabel()
+
+        dialog_layout.addLayout(hotkey_hbox)
+        dialog_layout.addWidget(self.error_label)
         dialog_layout.addWidget(self.load_button)
         self.setLayout(dialog_layout)
 
