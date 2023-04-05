ca03cc6d112d6f5b1bc5011ca5e219dc19000b82
Briggs-Richardson <briggsbdr@gmail.com>
2023-04-05 11:37:42 -0700

    Sets timestamp width dependent slider margins
    Before this patch, the sliders that make up the scalable scrubbing bar
    occupied as much vertical space as possible in the horizontal layouts
    they were placed in. In order to fit timestamp labels on the extreme
    ends of a slider, or in their future tick-mark bars, we need extra space
    on their left and right sides. This patch adds margins, based on the
    maximum width of a timestamp label, to the sides of each slider.
    
    Testing Steps:
      1. Run the application
      2. Verify that the slider labels are left aligned
      3. Verify that each slider has the same left and right margins
      4. Verify that the timestamp labels in the scaling bar are not cut off
      5. Load a video
      6. Verify that the functionality of the sliders work as expected
    
    Type: New Feature

--- View/scrubber_bar.py
+++ View/scrubber_bar.py
@@ -1,7 +1,6 @@
-from PySide6.QtCore import Qt, QPoint
-from PySide6.QtGui import QTransform
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QStackedLayout, QGridLayout, QHBoxLayout, QLabel
-from superqt import QRangeSlider, QLabeledRangeSlider
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QTransform, QFontMetrics
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QHBoxLayout, QLabel
 
 from View.progress_bar_view import ProgressBarView
 from View.scaling_bar import ScalingBar
@@ -22,48 +21,63 @@ class ScrubberBar(QWidget):
         """
         super().__init__()
 
-        self.scaling_bar = QLabeledRangeSlider(Qt.Orientation.Horizontal)
-        self.scaling_bar.setEdgeLabelMode(QLabeledRangeSlider.LabelPosition.NoLabel)
-        self.scaling_bar = ScalingBar()
+        # Since the timestamp label width will vary depending on font, we
+        # compute the maximum width required to represent all timestamp labels.
+        timestamp_width = self._compute_max_timestamp_width()
+        horizontal_margin = timestamp_width // 2
+
+        # Create and configure the scaling bar.
+        self.scaling_bar_widget = ScalingBar(timestamp_width)
+        self.scaling_bar = self.scaling_bar_widget.get_slider()
         self.scaling_bar.setBarMovesAllHandles(True)
         self.scaling_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
         self.scaling_bar.setValue((self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1]))
 
-        # This slider will be read-only, represents the progress for the scaling bar.
-        self.scaling_progress_view = QSlider(Qt.Orientation.Horizontal)
-        self.scaling_progress_view.setEnabled(False)
-
-        self.progress_bar_view = ProgressBarView()
-
-        progress_bar = self.get_progress_bar()
-        progress_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
-        progress_bar.setValue(self.DEFAULT_RANGE_BOUNDS[0])
-
-        self.slider_max = self.DEFAULT_RANGE_BOUNDS[1]
-
+        # Add a label and the scaling bar to a layout.
         scaling_bar_horizontal_layout = QHBoxLayout()
         scaling_bar_label = QLabel("Scaling Bar")
         scaling_bar_label.setFixedWidth(90)
+        scaling_bar_vertical_layout = QVBoxLayout()
+        scaling_bar_vertical_layout.addWidget(self.scaling_bar_widget)
         scaling_bar_horizontal_layout.addWidget(scaling_bar_label)
-        scaling_bar_horizontal_layout.addWidget(self.scaling_bar)
+        scaling_bar_horizontal_layout.addLayout(scaling_bar_vertical_layout)
 
+        # Create and configure the progress bar (read only).
+        self.scaling_progress_view = QSlider(Qt.Orientation.Horizontal)
+        self.scaling_progress_view.setEnabled(False)
+
+        # Add a label and the progress bar to a layout
         scaling_progress_view_horizontal_layout = QHBoxLayout()
         scaling_progress_view_label = QLabel("Progress Bar")
         scaling_progress_view_label.setFixedWidth(90)
+        scaling_progress_view_layout_vertical = QVBoxLayout()
+        scaling_progress_view_layout_vertical.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
+        scaling_progress_view_layout_vertical.addWidget(self.scaling_progress_view)
         scaling_progress_view_horizontal_layout.addWidget(scaling_progress_view_label)
-        scaling_progress_view_horizontal_layout.addWidget(self.scaling_progress_view)
+        scaling_progress_view_horizontal_layout.addLayout(scaling_progress_view_layout_vertical)
 
+        # Create and configure the scrubber bar
+        self.progress_bar_view = ProgressBarView()
+        progress_bar = self.get_progress_bar()
+        progress_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
+        progress_bar.setValue(self.DEFAULT_RANGE_BOUNDS[0])
+        self.slider_max = self.DEFAULT_RANGE_BOUNDS[1]
+
+        # Add a label and the scrubber bar to a layout
         progress_bar_horizontal_layout = QHBoxLayout()
         progress_bar_label = QLabel("Scrubber Bar")
         progress_bar_label.setFixedWidth(90)
+        progress_bar_vertical_layout = QVBoxLayout()
+        progress_bar_vertical_layout.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
+        progress_bar_vertical_layout.addWidget(self.progress_bar_view)
         progress_bar_horizontal_layout.addWidget(progress_bar_label)
-        progress_bar_horizontal_layout.addWidget(self.progress_bar_view)
+        progress_bar_horizontal_layout.addLayout(progress_bar_vertical_layout)
 
+        # Add all three bars to this widget's vertical layout
         vertical_layout = QVBoxLayout()
         vertical_layout.addLayout(scaling_bar_horizontal_layout)
         vertical_layout.addLayout(scaling_progress_view_horizontal_layout)
         vertical_layout.addLayout(progress_bar_horizontal_layout)
-
         self.setLayout(vertical_layout)
 
     def initialize(self, upper_bound):
@@ -111,3 +125,19 @@ class ScrubberBar(QWidget):
         mid_point = (lower_unit + (upper_unit - lower_unit) / 2) / self.slider_max
         self.progress_bar_view.setTransform(QTransform.fromScale(1 / width_scale, 1))
         self.progress_bar_view.centerOn(mid_point * w, self.get_progress_bar().height() / 2)
+
+    def _compute_max_timestamp_width(self):
+        """
+        Computes the maximum timestamp width required to paint any arbitrary
+        timestamp. The width of a timestamp will depend on the font used by
+        the system.
+        """
+        max_width = 0
+        font_metrics = QFontMetrics(self.font())
+
+        # Some digits are wider than other digits.
+        for digit in range(10):
+            timestamp_with_digit_only = f"{digit}{digit}:{digit}{digit}:{digit}{digit}.{digit}{digit}"
+            max_width = max(max_width, font_metrics.horizontalAdvance(timestamp_with_digit_only))
+
+        return max_width
