52ef35d6dee20b59735b7e1840961dd17e2b5daa
Briggs-Richardson <85253184+Briggs-Richardson@users.noreply.github.com>
2023-04-12 19:36:34 -0700

    Refactors and reimplements the scrubber bar (#60)
    * Refactors and reimplements the scrubber bar
    
    Before this patch, the scrubber bar was a QSlider placed in a
    QGraphicsScene and viewed in a QGraphicsView. Upon scaling the scrubber
    bar, the view would zoom in to the respective portion of the slider bar.
    While this did provide a scaling effect, it had some undesirable side
    effects. The most problematic was the handle width would zoom in
    addition to the bar. And, with the handle width becoming an unknown
    unretrievable quantity, it has become impossible to quantitate the exact
    width of the slider bar, a necessary factor in computing tick-marks.
    
    This patch reimplements the scrubber bar as a derived QSlider, which
    dynamically changes its range according to the scaling bar in order to
    scale, and which hides its handle using an overriden paintEvent method.
    Now, scaling should be possible, and in addition, the handle width is
    constant.
    
    This patch also refactors the scalable scrubbing bar files, moving all
    related files into a related folder for better organization.
    
    Testing Steps:
      1. Run the application
      2. Verify the scrubber bar handle can be moved within its current
         range
      3. Verify that the progress rect (the blue rectangle spanning from the
         beginning of the slider to the handle) moves with the handle
         appropriately.
      4. Load a video
      5. Verify that the scrubber bar works as expected when the scaling bar
         is set in its default state.
         5a. Verify the scrubber bar updates with the video
         5b. Verify updates in the scrubber bar update the video
      6. Change the scaling bar value and verify that the range of the
         scrubber bar accurately represents the scaling bar interval
      7. Verify that the handle is hidden when the progress bar handle
         represents a value outside the range of the scrubber bar.
         7a. When the progress bar handle is less than the minimum of the
         scrubber bar, the handle and progress rect should not be visible
         7b. When the progress bar handle is greater than the maximum of the
         scrubber bar, the handle should not be visible and the progress
         rect should occupy the entire width of the scrubber bar.
    
    Type: New Feature
    
    * Adds labeled scalable tick marks to sliders
    
    This patch adds a custom scalable tick-mark bar to each slider in the
    scalable scrubbing bar. The tick-mark bars make assumptions about the
    slider width, margins, and positioning. In the future, it would be
    reasonable to package the slider and tick mark bars into one container
    class, in order to get rid of the required assumptions.
    
    Testing Steps:
      1. Run the application
      2. Verify that there are no visible tick-marks under the bars
      3. Load a video
      4. Verify that the tick-mark bars are now present
      5. Verify the tick-mark bars of the scaling and progress bars.
         5a. The ticks should start at timestamp 0, and go up to,
             but may not include, the end of the video.
         5b. The ticks should be evenly spaced, and the text should
             not overlap.
         5c. The number of ticks should be maximized. Verify that
             the next smaller interval could not be used.
      6. Verify that the tick marks appropriately respond to scaling
         6a. As the scale increases, the tick intervals should
             become increasingly smaller
         6b. As the scale changes, the handle, when paused, should
             not change in visual value
         6c. The timestamps should reflect the scaled range of the
             scrubber bar
         6d. Dragging the scale bar should change the timestamps
             appropriately
    
    Type: New Feature

--- Controllers/window_controller.py
+++ Controllers/window_controller.py
@@ -84,10 +84,8 @@ class WindowController:
         self._window.media_panel.media_control_panel.play_pause_button.clicked.connect(
             self.play_video)
 
-        self._window.media_panel.scrubber_bar.get_progress_bar().sliderMoved.connect(
+        self._window.media_panel.scalable_scrubber_bar.scrubber_bar.onValueChanged.connect(
             self.update_video_on_progres_bar_movement)
-        self._window.media_panel.scrubber_bar.scaling_bar.valueChanged.connect(
-            self.update_progress_bar_range)
 
         self._media_player.positionChanged.connect(self.update_progress_bar_on_video_position_changed)
         self._media_player.durationChanged.connect(self.on_video_duration_changed)
@@ -160,9 +158,6 @@ class WindowController:
         Opens an input dialog box to request an initial title for the encoding
         table of the session. Then, sets the initial title to the user-response.
         """
-        # text, ok = QInputDialog.getText(self._window, "Encoding Table Title Name",
-        #                                 "Encoding Table Title:", QLineEdit.Normal, "")
-        # if ok and text:
         self._window.table_panel.title.setText(table_title)
         self.resize_to_content()
 
@@ -180,7 +175,7 @@ class WindowController:
         Parameters:
             new_duration - current duration of the video.
         """
-        self._window.media_panel.scrubber_bar.initialize(new_duration)
+        self._window.media_panel.scalable_scrubber_bar.initialize(new_duration)
 
     @Slot()
     def get_video_time_total(self):
@@ -360,19 +355,8 @@ class WindowController:
         Parameters:
             position - current position of the video
         """
-        self._window.media_panel.scrubber_bar.get_progress_bar().setValue(position)
-        self._window.media_panel.scrubber_bar.scaling_progress_view.setValue(position)
-
-    # Slot annotation is not present as range_bounds is a tuple, and that is not
-    # recognized by QT. However, the slot function still connects and executes
-    # regardless.
-    def update_progress_bar_range(self, range_bounds):
-        """
-        Updates the range of the progress bar based on the new values
-        of the scalable scrubbing bar.
-        """
-        lower_range, upper_range = range_bounds
-        self._window.media_panel.scrubber_bar.set_progress_zoom(lower_range, upper_range)
+        self._window.media_panel.scalable_scrubber_bar.progress_bar.setValue(position)
+        self._window.media_panel.scalable_scrubber_bar.scrubber_bar.setValue(position)
 
     @Slot()
     def set_cell_size(self):
--- View/ScalableScrubbingBar/scalable_scrubber_bar.py
+++ View/ScalableScrubbingBar/scalable_scrubber_bar.py
@@ -1,14 +1,15 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QTransform, QFontMetrics
+from PySide6.QtGui import QFontMetrics
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QHBoxLayout, QLabel
 
-from View.progress_bar_view import ProgressBarView
-from View.scaling_bar import ScalingBar
+from View.ScalableScrubbingBar.labeled_slider_tick_marks import LabeledSliderTickMarks
+from View.ScalableScrubbingBar.scaling_bar import ScalingBar
+from View.ScalableScrubbingBar.scrubber_bar import ScrubberBar
 
 
-class ScrubberBar(QWidget):
+class ScalableScrubberBar(QWidget):
     """
-    The ScrubbingBar is a custom widget, including a scaling bar and progress
+    The ScalableScrubberBar is a custom widget, including a scaling bar and progress
     bars, that when combined, form a functional scalable scrubbing bar.
     """
     DEFAULT_RANGE_BOUNDS = (0, 100)
@@ -24,6 +25,7 @@ class ScrubberBar(QWidget):
         # Since the timestamp label width will vary depending on font, we
         # compute the maximum width required to represent all timestamp labels.
         timestamp_width = self._compute_max_timestamp_width()
+        timestamp_width += 1 if timestamp_width % 2 == 1 else 0
         horizontal_margin = timestamp_width // 2
 
         # Create and configure the scaling bar.
@@ -32,53 +34,70 @@ class ScrubberBar(QWidget):
         self.scaling_bar.setBarMovesAllHandles(True)
         self.scaling_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
         self.scaling_bar.setValue((self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1]))
+        self.scaling_bar_tick_marks = LabeledSliderTickMarks(self.scaling_bar, timestamp_width)
 
         # Add a label and the scaling bar to a layout.
         scaling_bar_horizontal_layout = QHBoxLayout()
         scaling_bar_label = QLabel("Scaling Bar")
         scaling_bar_label.setFixedWidth(90)
         scaling_bar_vertical_layout = QVBoxLayout()
+        scaling_bar_vertical_layout.setSpacing(0)
         scaling_bar_vertical_layout.addWidget(self.scaling_bar_widget)
+        scaling_bar_vertical_layout.addWidget(self.scaling_bar_tick_marks)
         scaling_bar_horizontal_layout.addWidget(scaling_bar_label)
         scaling_bar_horizontal_layout.addLayout(scaling_bar_vertical_layout)
 
         # Create and configure the progress bar (read only).
-        self.scaling_progress_view = QSlider(Qt.Orientation.Horizontal)
-        self.scaling_progress_view.setEnabled(False)
+        self.progress_bar = QSlider(Qt.Orientation.Horizontal)
+        self.progress_bar.setEnabled(False)
+        self.progress_bar_tick_marks = LabeledSliderTickMarks(self.progress_bar, timestamp_width)
 
         # Add a label and the progress bar to a layout
-        scaling_progress_view_horizontal_layout = QHBoxLayout()
-        scaling_progress_view_label = QLabel("Progress Bar")
-        scaling_progress_view_label.setFixedWidth(90)
-        scaling_progress_view_layout_vertical = QVBoxLayout()
-        scaling_progress_view_layout_vertical.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
-        scaling_progress_view_layout_vertical.addWidget(self.scaling_progress_view)
-        scaling_progress_view_horizontal_layout.addWidget(scaling_progress_view_label)
-        scaling_progress_view_horizontal_layout.addLayout(scaling_progress_view_layout_vertical)
+        progress_bar_horizontal_layout = QHBoxLayout()
+        progress_bar_label = QLabel("Progress Bar")
+        progress_bar_label.setFixedWidth(90)
+        progress_bar_layout_vertical = QVBoxLayout()
+        progress_bar_layout_vertical.setSpacing(0)
+        progress_bar_inner_layout = QVBoxLayout()
+        progress_bar_inner_layout.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
+        progress_bar_inner_layout.addWidget(self.progress_bar)
+        progress_bar_layout_vertical.addLayout(progress_bar_inner_layout)
+        progress_bar_layout_vertical.addWidget(self.progress_bar_tick_marks)
+        progress_bar_horizontal_layout.addWidget(progress_bar_label)
+        progress_bar_horizontal_layout.addLayout(progress_bar_layout_vertical)
 
         # Create and configure the scrubber bar
-        self.progress_bar_view = ProgressBarView()
-        progress_bar = self.get_progress_bar()
-        progress_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
-        progress_bar.setValue(self.DEFAULT_RANGE_BOUNDS[0])
+        self.scrubber_bar = ScrubberBar(self.scaling_bar)
+        self.scrubber_bar.setRange(self.DEFAULT_RANGE_BOUNDS[0], self.DEFAULT_RANGE_BOUNDS[1])
+        self.scrubber_bar.setValue(self.DEFAULT_RANGE_BOUNDS[0])
         self.slider_max = self.DEFAULT_RANGE_BOUNDS[1]
+        self.scrubber_bar_tick_marks = LabeledSliderTickMarks(self.scrubber_bar, timestamp_width)
 
         # Add a label and the scrubber bar to a layout
-        progress_bar_horizontal_layout = QHBoxLayout()
-        progress_bar_label = QLabel("Scrubber Bar")
-        progress_bar_label.setFixedWidth(90)
-        progress_bar_vertical_layout = QVBoxLayout()
-        progress_bar_vertical_layout.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
-        progress_bar_vertical_layout.addWidget(self.progress_bar_view)
-        progress_bar_horizontal_layout.addWidget(progress_bar_label)
-        progress_bar_horizontal_layout.addLayout(progress_bar_vertical_layout)
+        scrubber_bar_horizontal_layout = QHBoxLayout()
+        scrubber_bar_label = QLabel("Scrubber Bar")
+        scrubber_bar_label.setFixedWidth(90)
+        scrubber_bar_vertical_layout = QVBoxLayout()
+        scrubber_bar_vertical_layout.setSpacing(0)
+        scrubber_bar_inner_layout = QVBoxLayout()
+        scrubber_bar_inner_layout.setContentsMargins(horizontal_margin, 0, horizontal_margin, 0)
+        scrubber_bar_inner_layout.addWidget(self.scrubber_bar)
+        scrubber_bar_vertical_layout.addLayout(scrubber_bar_inner_layout)
+        scrubber_bar_vertical_layout.addWidget(self.scrubber_bar_tick_marks)
+        scrubber_bar_horizontal_layout.addWidget(scrubber_bar_label)
+        scrubber_bar_horizontal_layout.addLayout(scrubber_bar_vertical_layout)
 
         # Add all three bars to this widget's vertical layout
         vertical_layout = QVBoxLayout()
         vertical_layout.addLayout(scaling_bar_horizontal_layout)
-        vertical_layout.addLayout(scaling_progress_view_horizontal_layout)
         vertical_layout.addLayout(progress_bar_horizontal_layout)
+        vertical_layout.addLayout(scrubber_bar_horizontal_layout)
         self.setLayout(vertical_layout)
+
+        # Hide the tick-mark bars until a video is loaded.
+        self.scaling_bar_tick_marks.hide()
+        self.progress_bar_tick_marks.hide()
+        self.scrubber_bar_tick_marks.hide()
 
     def initialize(self, upper_bound):
         """
@@ -94,37 +113,13 @@ class ScrubberBar(QWidget):
         self.scaling_bar.setValue((0, upper_bound))
         self.slider_max = upper_bound
 
-        self.scaling_progress_view.setRange(0, upper_bound)
+        self.progress_bar.setRange(0, upper_bound)
+        self.scrubber_bar.setRange(0, upper_bound)
 
-        progress_bar = self.get_progress_bar()
-        progress_bar.setRange(0, upper_bound)
-        progress_bar.setValue(0)
-
-        self.set_progress_zoom(0, upper_bound)
-
-    def get_progress_bar(self):
-        """
-        Getter method that returns a reference to the progress bar.
-
-        Return:
-            reference to the progress bar
-        """
-        return self.progress_bar_view.get_progress_bar()
-
-    def set_progress_zoom(self, lower_unit, upper_unit):
-        """
-        Scales and zooms the progress bar to reflect the range of the scaling
-        bar.
-
-        Parameters:
-            lower_unit - unit value (ms) of left handle of scaling bar
-            upper_unit - unit value (ms) of right handle of scaling bar
-        """
-        w, h = self.get_progress_bar().width(), self.get_progress_bar().height()
-        width_scale = (upper_unit - lower_unit) / self.slider_max
-        mid_point = (lower_unit + (upper_unit - lower_unit) / 2) / self.slider_max
-        self.progress_bar_view.setTransform(QTransform.fromScale(1 / width_scale, 1))
-        self.progress_bar_view.centerOn(mid_point * w, self.get_progress_bar().height() / 2)
+        # Display the tick-mark bars
+        self.scaling_bar_tick_marks.show()
+        self.progress_bar_tick_marks.show()
+        self.scrubber_bar_tick_marks.show()
 
     def _compute_max_timestamp_width(self):
         """
@@ -133,7 +128,9 @@ class ScrubberBar(QWidget):
         the system.
         """
         max_width = 0
-        font_metrics = QFontMetrics(self.font())
+        font = self.font()
+        font.setPointSize(10)
+        font_metrics = QFontMetrics(font)
 
         # Some digits are wider than other digits.
         for digit in range(10):
